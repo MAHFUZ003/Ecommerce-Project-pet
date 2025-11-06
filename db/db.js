fa5d718678bbdf32178c8a99a5be7ce87e283e6e
Mahfuz <mahfuzy555@gmail.com>
2025-11-06 18:41:33 +0600

    up

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+// Test connection
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error(' Error connecting to PostgreSQL', err.stack);
+  }
+  console.log(' PostgreSQL connected successfully');
+  release(); 
+});
+
+module.exports = pool;
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path');
+require('dotenv').config()
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Welcome to My Shop' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
