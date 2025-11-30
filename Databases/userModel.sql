2fc41333440ad9c469bd8de08aa3d4cbcc29ffbc
Mahfuz <mahfuzy555@gmail.com>
2025-11-30 10:22:17 +0600

    Update userModel.sql

--- Databases/userModel.sql
+++ Databases/userModel.sql
@@ -2,6 +2,6 @@ CREATE TABLE users
 (
     id SERIAL PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,  -- increased length for hashed passwords
+    password VARCHAR(255) NOT NULL, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
